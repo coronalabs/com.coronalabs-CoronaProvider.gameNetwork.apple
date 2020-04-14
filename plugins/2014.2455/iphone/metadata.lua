d7045b8276e7e5f7870d7733acb3bc5c05453c19
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 22:59:21 -0400

    Adding binaries

---
+++ plugins/2014.2455/iphone/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'gameNetwork-apple' },
+		frameworks = { 'CoreTelephony'},
+		frameworksOptional = {},
+	},
+}
+
+return metadata
